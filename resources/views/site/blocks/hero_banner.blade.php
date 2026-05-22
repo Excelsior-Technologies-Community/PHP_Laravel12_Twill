cf0ceda10777b4a2046bc9b758237fbf8c73ba6d
savasanisujal1 <savasanisujal1@gmail.com>
2026-05-22 17:00:30 +0530

    added Hero Banner block with desktop/mobile media cropping

---
+++ resources/views/site/blocks/hero_banner.blade.php
@@ -0,0 +1,4 @@
+<div class="hero-banner" style="background-image: url('{{ $block->image('cover') }}')">
+    <h1>{{ $block->input('title') }}</h1>
+    <p>{{ $block->input('subtitle') }}</p>
+</div>
---
+++ resources/views/twill/blocks/hero_banner.blade.php
@@ -0,0 +1,18 @@
+@twillBlockTitle('Hero Banner')
+@twillBlockIcon('image')
+@twillBlockGroup('app')
+
+@formField('input', [
+    'name' => 'title',
+    'label' => 'Title',
+])
+
+@formField('input', [
+    'name' => 'subtitle',
+    'label' => 'Subtitle',
+])
+
+@formField('medias', [
+    'name' => 'cover',
+    'label' => 'Cover Image',
+])
